993dade54c2e7cb77fb5e6a0105f171d4eab92a9
dabl03 <66857879+dabl03@users.noreply.github.com>
2021-05-07 16:48:36 -0400

    Update compilar.sh

--- compilar.sh
+++ compilar.sh
@@ -1,5 +1,5 @@
 #!system/bin/sh
 #no se si en linux existe PDcurses
 #Si no funciona cambielo por: lncurse
-gcc -Wall -lPDcurse -lpthread -ansi -g3 -o Proyecto_consola PingPong.C
+gcc -Wall -lPDcurse -lpthread -ansi -g3 -o Proyecto_consola PingPong.c
 exit

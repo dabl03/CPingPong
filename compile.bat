f42f12bf25330581ee8608c93af1250e29035705
dabl03 <66857879+dabl03@users.noreply.github.com>
2021-05-07 16:47:52 -0400

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
-	gcc PingPong.C -o Proyecto_consola.exe -Wall -ansi -g3 -lpthread -lPDcurses
+	gcc PingPong.c -o Proyecto_consola.exe -Wall -ansi -g3 -lpthread -lPDcurses
 	strip Proyecto_consola.exe
 	echo Eso es todo amigo.
 	if not %errorlevel%==0 ( PAUSE )
